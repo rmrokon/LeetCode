0f0e8bca3aab6b3f03f352730bf99afe153071ba
Md. Roknuzzaman <53700865+rmrokon@users.noreply.github.com>
2022-08-05 18:48:30 +0600

    Time: 9 ms (98.77%), Space: 41.5 MB (96.60%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reversedNum = reverseNum(x);
+        return x == reversedNum;
+    }
+    
+    int reverseNum(int num){
+        int reversedNumber = 0;
+        while(num > 0){
+            int currentDigit = num % 10;
+            reversedNumber = reversedNumber * 10 +  currentDigit;
+            num /= 10;
+        }
+        return reversedNumber;
+    }
+}
