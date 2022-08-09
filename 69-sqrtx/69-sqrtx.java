b9c11ff615e73d356073613561041b9518befcd0
Md. Roknuzzaman <53700865+rmrokon@users.noreply.github.com>
2022-08-10 02:51:59 +0600

    Time: 2 ms (84.17%), Space: 42.1 MB (6.55%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -3,17 +3,12 @@ class Solution {
       if(x == 0){
           return 0;
       }
-        long[] perfectSquare = binarySearch(x);
-        if(perfectSquare[0] != -1){
-            return (int)(perfectSquare[0]);
-        }
-        
-        return (int)(perfectSquare[1]);
+    return (int)(binarySearch(x));
            
         
     }
     
-    long[] binarySearch(int num){
+    long binarySearch(int num){
         long start = 1;
         long end = num;
         
@@ -21,7 +16,7 @@ class Solution {
             long mid = start + (end - start)/2;
             
             if(mid * mid == num){
-                return new long[] {mid};             
+                return mid;             
             }
             if(mid * mid > num){
                 end = mid -1;
@@ -29,6 +24,6 @@ class Solution {
                 start = mid + 1;
             }
         }
-        return new long[] {-1, end};
+        return end;
     }
 }
