183c8a69723989217db205ed83c1789a8d753f7c
Md. Roknuzzaman <53700865+rmrokon@users.noreply.github.com>
2022-08-16 01:43:22 +0600

    Time: 0 ms (100.00%), Space: 41.1 MB (34.79%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int start = 1;
+        int end = num;
+        
+        while(start <= end){
+            long mid = start + (end - start)/2;
+            
+            if(mid * mid == num){
+                return true;
+            }
+            if(mid * mid < num){
+                start = (int) mid + 1;
+            }
+            else{
+                end = (int) mid - 1;
+            }
+            
+        }
+        return false;
+    }
+}
